e44652432790c98d8f9cbd961d4171445d58d396
ashleybrown22343-max <ashleybrown22343@gmail.com>
2026-09-03 22:10:03 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,1 @@
-// NUCLEAR CACHE WIPER: Deletes all old cache, forces network load.
-self.addEventListener('install', (e) => {
-    self.skipWaiting();
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(cacheNames.map(cacheName => caches.delete(cacheName)));
-        })
-    );
-    self.clients.claim();
-});
-
-// No caching at all. Always fetch from Vercel.
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request));
-});
+// Intentionally empty to stop blocking the app
